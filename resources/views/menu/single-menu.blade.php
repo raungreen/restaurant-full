b540e0c4f875c49a0644436b2c19fdd8ebb4c9a4
Raun G <rgreen@creativeanvil.com>
2020-07-06 12:47:56 -0500

    created single menu page html and css

---
+++ resources/views/menu/single-menu.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="single-menu-page">
+      <div class="row">
+        <div class="col-sm-12 offset-md-1 col-md-10 offset-lg-2 col-lg-8">
+          <div class="content-box">
+            <div class="row">
+            <div class="col-md-12">
+              <h1>Burger</h1>
+            </div>
+              <div class="col-md-6">
+                <div class="item">
+                  <div class="title">
+                    <h4>Texas Burger</h4>
+                    <span class="price">$9</span>
+                  </div>
+                  <div class="description">
+                    <p>French bun, Ground Sirloin, Tomato, American cheese, BBQ sauce </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="item">
+                  <div class="title">
+                    <h4>Texas Burger</h4>
+                    <span class="price">$9</span>
+                  </div>
+                  <div class="description">
+                    <p>French toast bun, Ground Sirloin, Tomato, American cheese, BBQ sauce </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="item">
+                  <div class="title">
+                    <h4>Texas Burger</h4>
+                    <span class="price">$9</span>
+                  </div>
+                  <div class="description">
+                    <p>French toast bun, Ground Sirloin, Tomato, American cheese, BBQ sauce </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="item">
+                  <div class="title">
+                    <h4>Texas Burger</h4>
+                    <span class="price">$9</span>
+                  </div>
+                  <div class="description">
+                    <p>French toast bun, Ground Sirloin, Tomato, American cheese, BBQ sauce </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
