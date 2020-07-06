0e44c59b1971eb7c1235562c630e00b2cb6503bc
Raun G <rgreen@creativeanvil.com>
2020-07-06 10:51:20 -0500

    first commit

---
+++ resources/views/includes/side-menu.blade.php
@@ -0,0 +1,41 @@
+<div class="side-menu">
+  <a href="/" class="logo">
+    <img src="/img/restaurant-logo-5.png">
+  </a>
+  <div class="menu">
+    <div class="menu-title">
+      Menu
+    </div>
+    <ul class="links">
+      <li>
+        <a href="/menu">Food</a>
+      </li>
+      <li>
+        <a href="/about">About Us</a>
+      </li>
+      <li>
+        <a href="/waitlist">Waitlist</a>
+      </li>
+      <li>
+        <a href="/offers">Offers</a>
+      </li>
+      
+    </ul>
+  </div>
+  <div class="social-icons">
+    <a href="#">
+      <i class="fa fa-facebook" aria-hidden="true"></i>
+    </a>
+    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+  </div>
+  <div class="location">
+    <div class="address">
+      234 Main Street,<br>
+      St. Louis, Missouri 63047
+    </div>
+    <div class="phone-number">
+      <a href="tel:3142198652">314-219-8652</a>
+    </div>
+  </div>
+</div>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="about-page">
+    <div class="content-box">
+    <div class="row">
+    <div class="col-md-6">
+      <h1>About Page </h1>
+      <p>
+        I'm baby humblebrag chartreuse fashion axe disrupt photo booth tofu woke schlitz messenger bag vaporware church-key. Cliche pop-up hoodie pinterest, tote bag farm-to-table shabby chic literally tumeric kickstarter viral. Street art mixtape blog copper mug franzen, organic fanny pack la croix stumptown you probably haven't heard of them pinterest fashion axe VHS raw denim. Lomo lo-fi williamsburg, adaptogen flexitarian sustainable roof party irony banh mi PBR&B 3 wolf moon. Tote bag PBR&B fashion axe iPhone organic butcher hammock vice umami YOLO. Meh tote bag master cleanse church-key, franzen umami migas tilde chicharrones knausgaard mumblecore williamsburg lyft.
+      </p>
+      <p>
+        I'm baby humblebrag chartreuse fashion axe disrupt photo booth tofu woke schlitz messenger bag vaporware church-key. Cliche pop-up hoodie pinterest, tote bag farm-to-table shabby chic literally tumeric kickstarter viral. Street art mixtape blog copper mug franzen, organic fanny pack la croix stumptown you probably haven't heard of them pinterest fashion axe VHS raw denim. Lomo lo-fi williamsburg, adaptogen flexitarian sustainable roof party irony banh mi PBR&B 3 wolf moon. Tote bag PBR&B fashion axe iPhone organic butcher hammock vice umami YOLO. Meh tote bag master cleanse church-key, franzen umami migas tilde chicharrones knausgaard mumblecore williamsburg lyft.
+      </p>
+      </div>
+        <div class="col-md-6">
+          <img src="/img/about-burgers.jpeg" class="img-fluid">
+        </div>
+      </div>
+      <div>
+    </div>
+@endsection
