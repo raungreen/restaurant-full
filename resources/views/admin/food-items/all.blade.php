861e793064a7a934e48b35869607e46a8c0ef5c3
Raun G <rgreen@creativeanvil.com>
2020-07-09 14:47:15 -0500

    able to view all food items and edit all food items

--- resources/views/admin/food-items/all.blade.php
+++ resources/views/admin/food-items/all.blade.php
@@ -39,26 +39,29 @@
                                                 <th scope="col">id</th>
                                                 <th scope="col">Title</th>
                                                 <th scope="col">Price</th>
-                                                <th scope="col">Date Created</th>
+                                                <th scope="col">Date</th>
                                                 <th scope="col">Edit</th>
                                                 <th scope="col">Delete</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>Burgers</td>
-                                                <td>$9.00</td>
-                                                <td>2/2/2020</td>
-                                                <td>
-                                                    <a href="/admin/food-items/1/edit"><i class="far fa-edit"></i></a>
-                                                </td>
-                                                <td>
-                                                    <a href="/admin/food-items/1/delete" 
-                                                    onclick="if (! confirm('Are you sure you want to delete item?')) { return false; }">
-                                                    <i class="far fa-trash-alt"></a></i>
-                                                </td>
-                                            </tr>
+                                            @foreach ($items as $item)
+                                                
+                                                <tr>
+                                                    <th scope="row">{{$item->id}}</th>
+                                                    <td>{{$item->title}}</td>
+                                                    <td>{{$item->price}}</td>
+                                                    <td>{{date('m/d/y', strtotime($item->updated_at))}}</td>
+                                                    <td>
+                                                        <a href="/admin/food-items/{{$item->id}}/edit"><i class="far fa-edit"></i></a>
+                                                    </td>
+                                                    <td>
+                                                        <a href="/admin/food-items/{{$item->id}}/delete" 
+                                                        onclick="if (! confirm('Are you sure you want to delete item?')) { return false; }">
+                                                        <i class="far fa-trash-alt"></a></i>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
                                         </tbody>
                                     </table>
                                 </div>
