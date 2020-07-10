83fb5a6aa66ad794ca8048dd545c5b06e1cbdba2
Raun G <rgreen@creativeanvil.com>
2020-07-09 19:36:41 -0500

    Created new member from front page

---
+++ resources/views/pages/thank-you.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="offers-page">
+      <div class="content-box">
+        <div class="row">
+          <div class="col-md-8 offset-md-2 thank-you">
+            <h1>Thank You</h1>
+            <p>
+              You will be notified of all the special offers we have through email.
+            </P>
+        </div>
+      </div>
+      <div>
+    </div>
+@endsection
